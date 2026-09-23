043d797aac88031b7c6ffd7754a004777d51b103
jesusosanchez <jesusosanchez@gmail.com>
2026-09-23 02:13:21 -0400

    Update Wed 09-23-2026  2 13

--- js/indexpages.js
+++ js/indexpages.js
@@ -306,7 +306,7 @@ function populateTickerTable() {
               tdExtra.textContent = item[col] !== undefined ? item[col] : "";
             }
           } 
-          else if (col === "% Pfrt") {
+          else if (col === "% Prft" || col === "% Pfrt") {
             // Parse numeric value and apply color logic
             let value = parseFloat(item[col]);
         
